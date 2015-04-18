1d094b4acdd00810c836d1ff4d89a4cd12ba1f28
William Daniels <willbdaniels@gmail.com>
2015-04-18 12:02:39 -0600

    Added in some sample code for serial

--- java-serial/src/main/java/edu/csci/team3/Main.java
+++ java-serial/src/main/java/edu/csci/team3/Main.java
@@ -3,20 +3,99 @@ package edu.csci.team3;
 import gnu.io.CommPort;
 import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
-
-import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+public class Main {
 
-public class Main{
+    public Main() {
+        super();
+    }
 
-	public static void main(String[] args){
-		System.out.println("hello!");
-	}
+    void connect(String portName) throws Exception {
 
-private void connect ( String portName) throws Exception{
-	CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifer(portName);
-	if (portIdentifier.isCurrentlyOwned()){
+        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+        if (portIdentifier.isCurrentlyOwned()) {
+            System.out.println("Error: Port is currently in use");
+        } else {
+            CommPort commPort = portIdentifier.open(this.getClass().getName(), 2000);
 
+            if (commPort instanceof SerialPort) {
+                SerialPort serialPort = (SerialPort) commPort;
+                serialPort.setSerialPortParams(57600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+
+                InputStream in = serialPort.getInputStream();
+                OutputStream out = serialPort.getOutputStream();
+
+                (new Thread(new SerialReader(in))).start();
+                (new Thread(new SerialWriter(out))).start();
+
+            } else {
+                System.out.println("Error: Only serial ports are handled by this example.");
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public static class SerialReader implements Runnable {
+
+        InputStream in;
+
+        public SerialReader(InputStream in) {
+            this.in = in;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            try {
+                while ((len = this.in.read(buffer)) > -1) {
+                    System.out.print(new String(buffer, 0, len));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public static class SerialWriter implements Runnable {
+
+        OutputStream out;
+
+        public SerialWriter(OutputStream out) {
+            this.out = out;
+
+        }
+
+        @Override
+        public void run() {
+            try {
+                int c = 0;
+                while ((c = System.in.read()) > -1) {
+                    this.out.write(c);
+                }
+            } catch (IOException e) {
+                e.printStackTrace(System.err);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+
+            String SerialPortID = "/dev/ttyACM0";
+            System.setProperty("gnu.io.rxtx.SerialPorts", SerialPortID);
+            System.out.println("List of ports: " + CommPortIdentifier.getPortIdentifiers());
+
+            (new Main()).connect("");
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
+}
