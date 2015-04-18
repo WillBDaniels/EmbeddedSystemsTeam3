f9329860eeaf4618cd2d94ab19a5baf1286795a0
William Daniels <willbdaniels@gmail.com>
2015-04-18 08:09:11 +0000

    Committing so others can work on it if needed

--- python_programs/test.py
+++ python_programs/test.py
@@ -23,38 +23,40 @@ GPIO_ECHO = 18
 
 print "Ultrasonic Measurement"
 
-# Set pins as output and input
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
-GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+while 1==1:
+	# Set pins as output and input
+	GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
+	GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+	
+	# Set trigger to False (Low)
+	GPIO.output(GPIO_TRIGGER, False)
+	
+	# Allow module to settle
+	time.sleep(0.5)
+	
+	# Send 10us pulse to trigger
+	GPIO.output(GPIO_TRIGGER, True)
+	time.sleep(0.00001)
+	GPIO.output(GPIO_TRIGGER, False)
+	start = time.time()
+	while GPIO.input(GPIO_ECHO)==0:
+	  start = time.time()
+	
+	while GPIO.input(GPIO_ECHO)==1:
+	  stop = time.time()
+	
+	# Calculate pulse length
+	elapsed = stop-start
+	
+	# Distance pulse travelled in that time is time
+	# multiplied by the speed of sound (cm/s)
+	distance = elapsed * 34000
+	
+	# That was the distance there and back so halve the value
+	distance = distance / 2
 
-# Set trigger to False (Low)
-GPIO.output(GPIO_TRIGGER, False)
+	print "Distance : %.1f" % distance
 
-# Allow module to settle
-time.sleep(0.5)
-
-# Send 10us pulse to trigger
-GPIO.output(GPIO_TRIGGER, True)
-time.sleep(0.00001)
-GPIO.output(GPIO_TRIGGER, False)
-start = time.time()
-while GPIO.input(GPIO_ECHO)==0:
-  start = time.time()
-
-while GPIO.input(GPIO_ECHO)==1:
-  stop = time.time()
-
-# Calculate pulse length
-elapsed = stop-start
-
-# Distance pulse travelled in that time is time
-# multiplied by the speed of sound (cm/s)
-distance = elapsed * 34000
-
-# That was the distance there and back so halve the value
-distance = distance / 2
-
-print "Distance : %.1f" % distance
 
 # Reset GPIO settings
 GPIO.cleanup()
